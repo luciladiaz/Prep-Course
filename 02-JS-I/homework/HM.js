9e7739eb610eb899137c855073e72df6a86a5fc1
Lucila <luucila20@gmail.com>
2022-05-05 10:51:19 -1200

    hm sublime

---
+++ 02-JS-I/homework/HM.js
@@ -0,0 +1,16 @@
+VAR : es una manera de almacenar algo . Podemos almacenar muchas cosas como palabras, numeros, letras.
+
+STRINGS: es un tipo de dato, son caracteres.
+
+FUNCIONES : sirven para facilitar el codigo cuando tenemos que hacer varias cosas con un mismo dato 
+ ARGUMENTOS: son los datos que se le envian a la funcion para que se pueda ejecutar. Muchas funciones funcionan de igual manera sin argumentos, va a depender de lo que necesitemos hacer. 
+ RETURN: este comando sirve para retornar el valor que querramos mostrar en pantalla. 
+
+ IF : es un condicional. Esto significa que fuerza a que algo pase si o si. Si lo que esta dentro del if se cumple retorna un valor, si no se cumple sale por el else y retorna otro valor. 
+
+ VALORES BOOLEANOS: Son un tipo de dato que lo unico que retornan es true o false. Suponiendo que tenemos una condicion que se cumple, vamos a retornar TRUE, si esa condicion no se cumple retornamos FALSE. 
+ EJ: mi nombre es Lucila ? 
+     return TRUE. 
+
+     mi nombre es Antonia ? 
+     return FALSE. 
